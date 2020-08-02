219f773935caf30ff0068fde5a7afe5c3fda061e
Maroun Barqawi <62679040+El-lobo-cimarron@users.noreply.github.com>
2020-08-02 12:15:19 -0700

    Portfolio section and Bg pic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+  $("#year").text( new Date().getFullYear());
 
   // About me animation
   $("#aboutMeText").css("display", "none"); //Hiding About Me Text on page load
@@ -44,32 +44,6 @@ $(document).ready(function(){
 
 
 
-  // Problem with sliding to the portfolio section
-
-  // $(document).ready(function(){
-  //   // Add smooth scrolling to all links
-  //   $("a").on('click', function(event) {
-  //
-  //     // Make sure this.hash has a value before overriding default behavior
-  //     if (this.hash !== "portfolio") {
-  //       // Prevent default anchor click behavior
-  //       event.preventDefault();
-  //
-  //       // Store hash
-  //       var hash = this.hash;
-  //
-  //       // Using jQuery's animate() method to add smooth page scroll
-  //       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-  //       $('html, body').animate({
-  //         scrollTop: $(hash).offset().top
-  //       }, 800, function(){
-  //
-  //         // Add hash (#) to URL when done scrolling (default click behavior)
-  //         window.location.hash = hash;
-  //       });
-  //     } // End if
-  //   });
-  // });
 
 
 });
