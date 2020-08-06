2397d09a3312b4605b72db89d372c22b4b58ccc3
Maroun Barqawi <62679040+El-lobo-cimarron@users.noreply.github.com>
2020-08-06 16:39:34 -0700

    Fixed h1 animation

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
   $("#year").text( new Date().getFullYear());
 
+  //myName animation
+  $(".white-box:nth-child(2)").mouseenter(function(e){
+    $(".fName").addClass("fName-hover");
+    $(".fName").html("Maroon");
+  }).mouseleave(function(e){
+      $(".fName").removeClass("fName-hover");
+      $(".fName").html("Maroun");
+  });
+  $(".white-box:nth-child(3)").mouseenter(function(e){
+    $(".mName").addClass("mName-hover");
+  }).mouseleave(function(e){
+      $(".mName").removeClass("mName-hover");
+  });
+
+    // $(".mName").addClass("mName-hover");
+
   // About me animation
   $("#aboutMeText").css("display", "none"); //Hiding About Me Text on page load
   $("#aboutMe").click(function(){
@@ -12,9 +28,11 @@ $(document).ready(function(){
     if (clickNum%2 ) { //Odd (Home)
       // White boxes move to the right
       $(".white-box").removeClass("slide-left").addClass("slide-right");
+        $("#myName").removeClass("slide-left").addClass("slide-right");
         $(".slide-right").on('animationend', //removes the bug with animation
       function(){
         $(".white-box").removeClass("slide-right");
+        $("#myName").removeClass("slide-right");
       }
     );
       // About Me Text fading out
@@ -27,9 +45,11 @@ $(document).ready(function(){
     } else { // (About me)
       // White boxes move to the left
         $(".white-box").addClass("slide-left");
+        $("#myName").addClass("slide-left");
         $("#aboutMeText").css("display", "block");
       if(clickNum!==1){
         $(".white-box").removeClass("slide-right");
+        $("#myName").removeClass("slide-right");
         // About me text fading in
         $("#aboutMeText").removeClass("abm-end").addClass("abm-start");
       }
