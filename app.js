8a1c9d971c100b9f78e2916d2cdfc84cebc4a9e8
Maroun Barqawi <62679040+El-lobo-cimarron@users.noreply.github.com>
2020-08-14 00:14:40 -0700

    Mobile version (Portfolio needs fix)

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
   $("#year").text( new Date().getFullYear());
 
+  //Hamburger Nav Bar
+if (window.matchMedia("(max-device-width: 600px)").matches){
+    var navBar = "hiddenMenu";
+    $(".header a").css("display", "none");
+    $("#hamburger").click(function(){
+        if (navBar == "hiddenMenu"){
+          //$(".navbar-link").addClass("hmb-slideRight");
+            $(".header a").css("display", "block");
+            navBar = "activeMenu";
+        } else {
+          $(".navbar-link").css("display", "none");
+          navBar = "hiddenMenu";
+        }
+  });
+}
   //myName animation
   $(".white-box:nth-child(2)").mouseenter(function(e){
     $(".fName").addClass("fName-hover");
@@ -26,34 +41,59 @@ $(document).ready(function(){
     $this.data('clickNum', ++clickNum);
 
     if (clickNum%2 ) { //Odd (Home)
-      // White boxes move to the right
-      $(".white-box").removeClass("slide-left").addClass("slide-right");
-        $("#myName").removeClass("slide-left").addClass("slide-right");
-        $(".slide-right").on('animationend', //removes the bug with animation
-      function(){
-        $(".white-box").removeClass("slide-right");
-        $("#myName").removeClass("slide-right");
+      // For Mobile
+      if (window.matchMedia("(max-device-width: 600px)").matches){
+        $("#myName").removeClass("name-fadeOut").addClass("name-fadeIn");
+        // About Me Text fading out
+          $("#aboutMeText").removeClass("abm-fadeIn").addClass("abm-fadeOut");
+          $("abm-end").on('animationend', //removes About me after animation end
+        function(){
+          $("#aboutMeText").removeClass("abm-end");
+        });
+      } else {
+        // For Desktop
+        // White boxes move to the right
+        $(".white-box").removeClass("slide-left").addClass("slide-right");
+          $("#myName").removeClass("slide-left").addClass("slide-right");
+          $(".slide-right").on('animationend', //removes the bug with animation
+        function(){
+          $(".white-box").removeClass("slide-right");
+          $("#myName").removeClass("slide-right");
+        }
+      );
+        // About Me Text fading out
+          $("#aboutMeText").removeClass("abm-start").addClass("abm-end");
+          $("abm-end").on('animationend', //removes About me after animation end
+        function(){
+          $("#aboutMeText").removeClass("abm-end");
+        });
       }
-    );
-      // About Me Text fading out
-        $("#aboutMeText").removeClass("abm-start").addClass("abm-end");
-        $("abm-end").on('animationend', //removes About me after animation end
-      function(){
-        $("#aboutMeText").removeClass("abm-end");
-      });
+
 
     } else { // (About me)
-      // White boxes move to the left
-        $(".white-box").addClass("slide-left");
-        $("#myName").addClass("slide-left");
+      // For Mobile
+      if(window.matchMedia("(max-device-width: 600px)").matches){
+        //my Name fadein
+        $("#myName").addClass("name-fadeOut");
         $("#aboutMeText").css("display", "block");
-      if(clickNum!==1){
-        $(".white-box").removeClass("slide-right");
-        $("#myName").removeClass("slide-right");
-        // About me text fading in
-        $("#aboutMeText").removeClass("abm-end").addClass("abm-start");
-      }
+        if(clickNum!==1){
+          // myName fadein
+          $("#myName").removeClass("name-fadeIn").addClass("name-fadeOut");
+          $("#aboutMeText").removeClass("abm-fadeOut").addClass("abm-fadeIn");
+        }
+      } else {
+        // White boxes move to the left
+          $(".white-box").addClass("slide-left");
+          $("#myName").addClass("slide-left");
+          $("#aboutMeText").css("display", "block");
+        if(clickNum!==1){ //Clicked 3 or more times
+          $(".white-box").removeClass("slide-right");
+          $("#myName").removeClass("slide-right");
+          // About me text fading in
+          $("#aboutMeText").removeClass("abm-end").addClass("abm-start");
 
+        }
+      }
     }
 
   $("#aboutMe").text(function(i, text){
